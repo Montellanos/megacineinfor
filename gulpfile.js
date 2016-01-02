96a15616ec2f730cb0482c21979af84cd4b7295f
Jorge Montellanos Díaz <jorgemontellanos@gmail.com>
2016-01-02 18:30:27 -0400

    por fin da

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('default', ['server', 'reload']);
 gulp.task('reload', function(){
 	livereload.listen();
     gulp.watch(['./resources/views/*.php'], function(file){
-    	livereload.changed(file.path);
+    	livereload.reload(file.path);
     });
 });
     
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">asdaohjojommmss montellas</div>
+                <div class="title">sjsajdasdfsdf</div>
             </div>
         </div>
     </body>
